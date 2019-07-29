8475ac3b65779b27a4b0463db467d2c75faf27d4
Leo <losoviz@gmail.com>
2019-07-29 17:51:21 +0530

    Show error message if installation unsuccessful

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Install PoP and WordPress through Composer:
-echo "Downloading and installing PoP and WordPress through Composer... (this may take a few minutes)"
+echo "Downloading and installing PoP and WordPress through Composer..."
+echo "(this may take a few minutes)"
 composer create-project leoloso/pop-api-wp $FOLDER_NAME dev-master
 
 # Install the must-use plugins:
@@ -10,10 +11,10 @@ composer install
 
 ## Check if WordPress is installed. If not, install it
 echo "Checking if WordPress is installed: "
+echo 
 if ! $(wp core is-installed); then
 
-    echo "WordPress is not installed yet"
-    echo "Installing WordPress through WP-CLI..."
+    echo "WordPress is not installed yet. Installing WordPress through WP-CLI..."
     
     # Configure wp-config.php through WP-CLI: (reference: https://developer.wordpress.org/cli/commands/config/set/)
     wp config set DB_NAME $DB_NAME #eg: database
@@ -29,18 +30,26 @@ if ! $(wp core is-installed); then
 
     # Update the site URL, adding "/wp"
     wp option update siteurl $SITE_URL_WITH_HTTP/wp
+
+    # Check if the installation was successful. If not, show an error message
+    if ! $(wp core is-installed); then
+        echo "⚠️Installation unsuccessful. Please check the error messages displayed in the console to solve the issue, and then try again."
+        echo "If you need help, please copy the console output and send it to Leo by email (leo@getpop.org), and he will try to help."
+        exit 1;
+    fi
 else
-    echo "WordPress is already installed!"
+    echo "✅WordPress is already installed!"
+    exit 0;
 fi
 
-echo
-echo "All should be set now! Please check that these URLs work fine:"
+echo "✅Installation successful! Please check that the following URLs work fine:"
 echo "############################################"
-echo "# WordPress site: $SITE_URL_WITH_HTTP"
-echo "# WordPress admin: $SITE_URL_WITH_HTTP/wp/wp-admin/"
-echo "# PoP API (REST for posts): $SITE_URL_WITH_HTTP/posts/?action=api&datastructure=rest"
+echo "🍎WordPress site: $SITE_URL_WITH_HTTP"
+echo "🍎WordPress admin: $SITE_URL_WITH_HTTP/wp/wp-admin/"
+echo "🍎PoP API (REST for posts): $SITE_URL_WITH_HTTP/posts/?action=api&datastructure=rest"
 echo "############################################"
 echo
-echo "If you like PoP, please don't forget to give us a star in GitHub 😀❤️"
-echo "https://github.com/leoloso/PoP"
+echo "If you like PoP, please consider giving the project a star in GitHub 😀❤️"
+echo "👉🏽https://github.com/leoloso/PoP"
 echo "Bye 👋, happy using PoP!"
+exit 0;
