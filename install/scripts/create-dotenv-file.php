c1b08bd13ecea6c72e8223c31a349d16c543f780
Leo <losoviz@gmail.com>
2019-08-02 15:03:24 +0530

    Set debug param from environment constant

--- install/scripts/create-dotenv-file.php
+++ install/scripts/create-dotenv-file.php
@@ -2,32 +2,34 @@
 /**
  * Create the .env configuration files with default values for DEV/PROD environments
  */
-$lines = []; 
+$lines = [];
 if ('$ENV' === 'PROD') {
     $lines = [
-        'USE_CACHE' => true, 
-        'FAIL_IF_SUBCOMPONENT_DATALOADER_IS_UNDEFINED' => false, 
-        'ENABLE_EXTRA_ROUTES_BY_PARAMS' => false, 
-        'DISABLE_CUSTOM_CMS_CODE' => false, 
-        'ENABLE_CONFIG_BY_PARAMS' => false, 
-        'DISABLE_API' => false, 
+        'USE_CACHE' => true,
+        'FAIL_IF_SUBCOMPONENT_DATALOADER_IS_UNDEFINED' => false,
+        'ENABLE_EXTRA_ROUTES_BY_PARAMS' => false,
+        'DISABLE_CUSTOM_CMS_CODE' => false,
+        'ENABLE_CONFIG_BY_PARAMS' => false,
+        'DISABLE_API' => false,
         'EXTERNAL_SITES_RUN_SAME_SOFTWARE' => false,
+        'IS_CONFIG_CACHE_DEBUG' => false,
     ];
-} else { 
+} else {
     $lines = [
-        'USE_CACHE' => false, 
-        'FAIL_IF_SUBCOMPONENT_DATALOADER_IS_UNDEFINED' => true, 
-        'ENABLE_EXTRA_ROUTES_BY_PARAMS' => false, 
-        'DISABLE_CUSTOM_CMS_CODE' => false, 
-        'ENABLE_CONFIG_BY_PARAMS' => false, 
-        'DISABLE_API' => false, 
+        'USE_CACHE' => false,
+        'FAIL_IF_SUBCOMPONENT_DATALOADER_IS_UNDEFINED' => true,
+        'ENABLE_EXTRA_ROUTES_BY_PARAMS' => false,
+        'DISABLE_CUSTOM_CMS_CODE' => false,
+        'ENABLE_CONFIG_BY_PARAMS' => false,
+        'DISABLE_API' => false,
         'EXTERNAL_SITES_RUN_SAME_SOFTWARE' => false,
+        'IS_CONFIG_CACHE_DEBUG' => true,
     ];
-} 
-$output = ''; 
-foreach($lines as $variable => $value) { 
-    $output .= $variable.'='.(is_bool($value) ? ($value ? "true" : "false") : $value).PHP_EOL; 
-} 
-$fp = fopen(dirname(dirname(dirname(__FILE__))).'/config/.env', 'w'); 
-fwrite($fp, $output); 
+}
+$output = '';
+foreach($lines as $variable => $value) {
+    $output .= $variable.'='.(is_bool($value) ? ($value ? "true" : "false") : $value).PHP_EOL;
+}
+$fp = fopen(dirname(dirname(dirname(__FILE__))).'/config/.env', 'w');
+fwrite($fp, $output);
 fclose($fp);
