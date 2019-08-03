1279f18fd555bd3c4dd61b4c03c22b1026359fc2
Leo <losoviz@gmail.com>
2019-08-03 10:15:55 +0530

    Added "Success!"

--- install/print-footer.sh
+++ install/print-footer.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+GREEN='\033[0;32m'
 ORANGE='\033[0;33m'
 NC='\033[0m' # No Color
 UNDERLINE=`tput smul`
 NOUNDERLINE=`tput rmul`
 
-echo
+echo "🥳 ${GREEN}Success!${NC}"
 echo "If you like PoP, please consider giving the project a star in GitHub 😀 ❤️"
 echo -e "👉🏽 ${ORANGE}${UNDERLINE}https://github.com/leoloso/PoP${NOUNDERLINE}${NC}"
 echo "Bye 👋 , happy using PoP!"
