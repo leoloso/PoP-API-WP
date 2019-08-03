ad56acaa01e3466d51f1086ea36ec284e4a3cced
Leo <losoviz@gmail.com>
2019-08-03 16:11:35 +0530

    Execute script directly in composer.json

--- install/pre-maybe-install-wp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Exit status 0 if WordPress installed, otherwise 1 (https://developer.wordpress.org/cli/commands/core/is-installed/)
-wp core is-installed
-export IS_WP_ALREADY_INSTALLED=$?
