4cec5b06a5b1c4a5a168eab8046c3f30df2b7609
Leo <losoviz@gmail.com>
2019-08-03 10:18:47 +0530

    Improved welcome message

--- install/install.sh
+++ install/install.sh
@@ -4,7 +4,7 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 # Install PoP and WordPress through Composer:
-echo -e "Downloading and installing PoP and WordPress through Composer... ${YELLOW}(this may take a few minutes)${NC}"
+echo -e "Downloading and installing the PoP API, and WordPress, through Composer... ${YELLOW}(this may take a few minutes)${NC}"
 composer create-project leoloso/pop-api-wp $FOLDER_NAME
 
 # Install the must-use plugins:
