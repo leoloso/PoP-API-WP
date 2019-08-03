3ab1abeac3b7778c7d2f166109fc708775bae65f
Leo <losoviz@gmail.com>
2019-08-03 14:48:48 +0530

    Execute scripts directly from composer.json

---
+++ install/post-maybe-install-wp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# If previously not installed, and now it is, append success message
+if [ $IS_WP_ALREADY_INSTALLED -eq 1 ]
+then
+    if $(wp core is-installed); then
+        ORANGE='\033[0;33m'
+        NC='\033[0m' # No Color
+        UNDERLINE=`tput smul`
+        NOUNDERLINE=`tput rmul`
+        echo -e "🍎 PoP API: ${ORANGE}${UNDERLINE}$SITE_URL_WITH_HTTP/posts/?action=api&datastructure=rest${NOUNDERLINE}${NC} (REST for posts)"
+    fi
+fi
