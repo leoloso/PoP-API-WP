bb0201e3b1748a60bef04ef5b4f03e3fd867324d
Leo <losoviz@gmail.com>
2019-09-07 12:16:39 +0800

    Added pretty permalinks for GraphQL/REST endpoints

--- install/scripts/prepare-htaccess.php
+++ install/scripts/prepare-htaccess.php
@@ -4,10 +4,25 @@
  * This rewrite configuration must be printed before the WordPress rewrite (# BEGIN WordPress ...)
  */
 $apiRewriteString = <<<EOD
-# Pretty permalinks for API endpoints: rewrite from /some-url/api/ to /some-url/?scheme=api
+# Pretty permalinks for API endpoints:
+# 1. GraphQL: /some-url/api/graphql/
+# 2. REST: /some-url/api/rest/
+# 3. PoP native: /some-url/api/
 <IfModule mod_rewrite.c>
 RewriteEngine On
 RewriteBase /
+
+# 1. GraphQL: Rewrite from /some-url/api/graphql/ to /some-url/?scheme=api&datastructure=graphql
+RewriteCond %{SCRIPT_FILENAME} !-d
+RewriteCond %{SCRIPT_FILENAME} !-f
+RewriteRule ^(.*)/api/graphql/?$ /$1/?scheme=api&datastructure=graphql [L,P,QSA]
+
+# 2. REST: Rewrite from /some-url/api/rest/ to /some-url/?scheme=api&datastructure=rest
+RewriteCond %{SCRIPT_FILENAME} !-d
+RewriteCond %{SCRIPT_FILENAME} !-f
+RewriteRule ^(.*)/api/rest/?$ /$1/?scheme=api&datastructure=rest [L,P,QSA]
+
+# 3. PoP native: Rewrite from /some-url/api/ to /some-url/?scheme=api
 RewriteCond %{SCRIPT_FILENAME} !-d
 RewriteCond %{SCRIPT_FILENAME} !-f
 RewriteRule ^(.*)/api/?$ /$1/?scheme=api [L,P,QSA]
